019b34d376b2b3ac40855f75cfa4def86a98e71f
Sean Berrios <berrios96sean@gmail.com>
2023-02-14 20:04:06 -0700

    added more functionality to bank

--- Assignments/Socket_Project/Java_Files/Bank.java
+++ Assignments/Socket_Project/Java_Files/Bank.java
@@ -1,10 +1,15 @@
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Bank {
-    public static void main(String[] args) {
-        try {
+    public static Map<String,Object> customers = new HashMap<>(); 
+    public static void main(String[] args) 
+    {
+        try 
+        {
             // Set up a UDP socket on a specific IP address and port
             InetAddress address = InetAddress.getByName("127.0.0.1"); // replace with desired IP address
             int port = 12345; // replace with desired port number
@@ -14,18 +19,38 @@ public class Bank {
             byte[] buffer = new byte[1024];
             DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
             boolean listening = true;
-            while (listening) {
+            while (listening) 
+            {
                 socket.receive(packet);
                 
                 // Process incoming message
                 String message = new String(packet.getData(), 0, packet.getLength());
                 System.out.println("Received message: " + message);
-                
+                String[] parsedMessage = message.split(" ");
+                String command = parsedMessage[0];
+                if (command.equals("open"))
+                {
+                    System.out.println("OPEN");
+                }
+                if (command.equals("new-cohort"))
+                {
+                    System.out.println("NEW-COHORT");
+                }
+                if (command.equals("delete-cohort"))
+                {
+                    System.out.println("DELETECOHORT");
+                }
+                if (command.equals("exit"))
+                {
+                    System.out.println("EXIT");
+                }
                 // Stop listening when a message is received
                 listening = false;
             }
             socket.close();
-        } catch (Exception e) {
+        }
+        catch (Exception e) 
+        {
             e.printStackTrace();
         }
     }
